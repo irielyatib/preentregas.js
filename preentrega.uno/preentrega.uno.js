ba423c18146ece0da4aa0e1a83b36faedd39cfbe
Iriel Yatib <yatibiriel@gmail.com>
2024-07-24 22:05:54 -0300

    Add files via upload

---
+++ preentrega.uno/preentrega.uno.js
@@ -0,0 +1,39 @@
+//declaracion de una variable y uso del IF
+let tuCompra = prompt("¿Le gustaría comprar alguna tote bag?")
+if(tuCompra === "si") {
+    alert("Muchas gracias por confiar en nosotros!")
+} else if( tuCompra == "si") {
+    alert("Muchas gracias por confiar en nosotros!")
+
+}else {
+     alert("Usted se lo pierde!")
+
+}
+
+
+//switch
+let diseñosTotes = prompt("Ingrese el diseño que le gustaría comprar")
+
+switch(diseñosTotes){
+    case "dillom":
+      alert("Elegiste la tote:Dillom");
+      break
+ case "lana del rey":
+     alert("Elegiste la tote: Lana del Rey");
+     break
+ case "nirvana":
+     alert("Elegiste la tote: Nirvana")
+ default:
+    alert("ERROR, debe ingresar el nombre del diseño en stock")
+    break
+}
+
+let confirmar = true
+   while(confirmar) {
+       alert("Por favor indique una respuesta incorrecta")
+       confirmar= confirm ("¿Confirma querer seguir en la web?"); 
+   } 
+
+   //Se perfectamente que estoy entregando equivalente a un -1 pero la verdad, no les voy a mentir, lo dejé para ultimo momento y a último momento me caí de una barra de dominadas en crossfit y me golpee de lleno el coxis, me dieron reposo pero no pedí certificado pensando que iba a llegar y poder entregar, pero la verdad no hice más que estar acostada ya que me duele sentarme. Prometo hacer otra entrega, ésta la hago para no perder la beca.
+   // Desde ya muchas gracias y mil disculpas. 
+ 
